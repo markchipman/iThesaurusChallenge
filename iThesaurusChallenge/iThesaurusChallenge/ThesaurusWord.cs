50ae002ef2d0674c12acd7b512560dad67d8c759
Mark Chipman <markchipman@gmail.com>
2017-08-27 12:55:19 -0600

    Added the thesaurusword object

---
+++ iThesaurusChallenge/iThesaurusChallenge/ThesaurusWord.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iThesaurusChallenge
+{
+    /// <summary>
+    /// Word used in lookup
+    /// </summary>
+    public class ThesaurusWord
+    {
+
+        /// <summary>
+        /// Current name
+        /// </summary>
+        private readonly string _name;
+
+        /// <summary>
+        /// List of synonym
+        /// </summary>
+        private List<string> _synonymsList;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThesaurusWord"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public ThesaurusWord(string name)
+        {
+            _name = name;
+            _synonymsList = new List<string>();
+        }
+
+        #region methods
+        /// <summary>
+        /// Gets the synonyms list
+        /// </summary>
+        /// <returns>Synonyms</returns>
+        public IEnumerable<string> GetSynonyms()
+        {
+            return _synonymsList;
+        }
+
+
+        /// <summary>
+        /// Gets the name
+        /// </summary>
+        /// <returns>Name</returns>
+        public string GetName()
+        {
+            return _name;
+        }
+
+        /// <summary>
+        /// Adds a new synonym to the synonyms list
+        /// </summary>
+        /// <param name="synonyms">The synonyms list</param>
+        public void AddSynonyms(IEnumerable<string> synonyms)
+        {
+
+        }
+
+        #endregion
+    }
+}
