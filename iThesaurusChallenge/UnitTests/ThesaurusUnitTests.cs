b4e112b76d9df091c157f3299d9957116d3b3ab6
Mark Chipman <markchipman@gmail.com>
2017-08-27 12:05:15 -0600

    Initial commit

---
+++ iThesaurusChallenge/UnitTests/ThesaurusUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using iThesaurusChallenge;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// ThesaurusTest class
+    /// </summary>
+    public class ThesaurusUnitTests
+    {
+        /// <summary>
+        /// Reusable thesaurus instance
+        /// </summary>
+        readonly Thesaurus _thesaurus = new Thesaurus();
+
+        #region AddSynonyms
+
+
+        #endregion
+
+        #region GetSynonyms
+
+
+        #endregion
+
+
+        #region GetWords 
+
+
+        #endregion
+    }
+}
+
---
+++ iThesaurusChallenge/iThesaurusChallenge/Thesaurus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iThesaurusChallenge.Interfaces;
+
+namespace iThesaurusChallenge
+{
+    public class Thesaurus : IThesaurus
+    {
+        public void AddSynonyms(IEnumerable<string> synonyms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetSynonyms(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetWords()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
