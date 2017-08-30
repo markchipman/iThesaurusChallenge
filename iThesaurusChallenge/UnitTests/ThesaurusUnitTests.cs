0c3343457582eb7dabbdd0297000d6f317bdede5
Mark Chipman <markchipman@gmail.com>
2017-08-29 22:07:20 -0600

    Added more unit tests

--- iThesaurusChallenge/UnitTests/ThesaurusUnitTests.cs
+++ iThesaurusChallenge/UnitTests/ThesaurusUnitTests.cs
@@ -42,12 +42,67 @@ namespace UnitTests
                 Assert.Equal(inputList.Count-1, synonyms.Count());
                 Assert.Contains(inputList[i], words.ToList()[i]);
             }
+            Assert.Equal(inputList, words); // Order is important
+        }
+
+        [Fact]
+        public void AddEmpty()
+        {
+            _thesaurus.AddSynonyms(new List<string>());
+            Assert.True(!_thesaurus.GetWords().Any());
+        }
+
+        [Fact]
+        public void AddNull()
+        {
+            _thesaurus.AddSynonyms(null);
+            Assert.True(!_thesaurus.GetWords().Any());
         }
 
         #endregion
 
         #region GetSynonyms
 
+        [Fact]
+        public void GetSynonymsSingleWord()
+        {
+            var inputList = new List<string> { "automobile" };
+
+            _thesaurus.AddSynonyms(inputList);
+            IEnumerable<string> synonyms = _thesaurus.GetSynonyms(inputList[0]);
+            Assert.Equal(0, synonyms.Count());
+            Assert.IsAssignableFrom<IEnumerable<string>>(synonyms);
+        }
+
+        [Fact]
+        public void GetSynonymsMultipleAdditions()
+        {
+            var inputList1 = new List<string> { "automobile", "car", "buggy", "station wagon"};
+            var inputList2 = new List<string> { "automobile", "station wagon" };
+
+            _thesaurus.AddSynonyms(inputList1);
+            _thesaurus.AddSynonyms(inputList2);
+            List<string> synonyms = _thesaurus.GetSynonyms("station wagon").ToList();
+
+            Assert.Equal(3, synonyms.Count());
+            Assert.Contains("automobile", synonyms);
+            Assert.Contains("car", synonyms);
+            Assert.Contains("buggy", synonyms);
+        }
+
+        [Fact]
+        public void GetSynonymsEmptyStringInput()
+        {
+            IEnumerable<string> synonyms = _thesaurus.GetSynonyms(string.Empty);
+            Assert.Equal(0, synonyms.Count());
+        }
+
+        [Fact]
+        public void GetSynonymsNullInput()
+        {
+            IEnumerable<string> synonyms = _thesaurus.GetSynonyms(null);
+            Assert.Equal(0, synonyms.Count());
+        }
 
         #endregion
 
